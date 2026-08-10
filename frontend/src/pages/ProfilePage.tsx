037bec9fc78cc2a64d12654c6b301e15abc33d8d
Travkaa52 <dimakasan028@gmail.com>
2026-08-11 00:18:20 +0300

    Update ProfilePage.tsx

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getInstallGuideUrl } from '@/utils/install'; // Укажите ваш правильный путь
 import { useToastStore } from '@/store/useToastStore';
 import {
   User,
